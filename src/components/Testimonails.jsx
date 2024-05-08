bbab1ace667f8cab328562a9a40e9cfbcfd95bf2
Samar <osmansamar367@gmail.com>
2024-05-08 16:42:38 +0200

    Updated  the maximum width to the element on larger screens

--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -6,7 +6,7 @@ import Gaming from "../assets/image-gaming-growth.jpg";
 const Testimonails = () => {
   return (
     // max-w-7xl will apply a maximum width to the element on larger screens
-    <div className="max-w-7xl  mt-10 mb-12 mx-auto px-2  text-left cursor-pointer">
+    <div className="max-w-7xl  mt-10 mb-12 mx-auto px-2  text-left cursor-pointer xl:max-w-8xl">
       {/* Testimonials Container */}
       <div className="flex flex-col  md:flex-row md:space-x-6">
         {/* Retro */}
