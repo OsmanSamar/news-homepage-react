0574c04a531a72ecfe453afbef9a9bf6ed7451e0
Samar <osmansamar367@gmail.com>
2024-05-20 11:04:26 +0200

    Updated the "w-32 " of img

--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -5,13 +5,15 @@ import Gaming from "../assets/image-gaming-growth.jpg";
 
 const Testimonails = () => {
   return (
-    // max-w-7xl will  xl:max-w-7xl md:max-w-5xl px-2 apply a maximum width to the element on larger screens
-    <div className=" flex items-center justify-between mx-auto mt-10 mb-12   text-left cursor-pointer md:ml-[6vw]   xl:ml-[10vw]   ">
+    <div
+      className=" flex items-center justify-between mx-auto mt-10 mb-12 text-left
+     cursor-pointer md:ml-[6vw]   xl:ml-[10vw]   "
+    >
       {/* Testimonials Container */}
       <div className="flex flex-col  md:flex-row md:space-x-6">
         {/* Retro */}
         <div className=" flex  items-center p-2   md:w-1/3">
-          <img src={Retro} alt="Retro img" className="w-28" />
+          <img src={Retro} alt="Retro img" className="w-32" />
           <div className="flex flex-col ml-4  mb-14">
             <h5 className="text-2xl  font-bold text-grayish-blue">01</h5>
             <h3 className="text-lg font-bold hover:text-soft-red">
@@ -24,7 +26,7 @@ const Testimonails = () => {
         </div>
         {/* Laptop */}
         <div className=" flex  items-center p-2  md:w-1/3">
-          <img src={Laptop} alt="Laptop img" className="w-28" />
+          <img src={Laptop} alt="Laptop img" className="w-32" />
           <div className="flex flex-col ml-4  mb-14">
             <h5 className="text-2xl font-bold text-grayish-blue">02</h5>
             <h3 className="text-lg font-bold hover:text-soft-red">
@@ -37,7 +39,7 @@ const Testimonails = () => {
         </div>
         {/* Gaming */}
         <div className=" flex  items-center p-2   md:w-1/3">
-          <img src={Gaming} alt="Gaming img" className="w-28 " />
+          <img src={Gaming} alt="Gaming img" className="w-32 " />
           <div className="flex flex-col ml-4 mb-14">
             <h5 className="text-2xl font-bold text-grayish-blue">03</h5>
             <h3 className="text-lg font-bold hover:text-soft-red">
