6fb0da093e2b18e3c2dc073a0dcf7a64a10ac715
Samar <osmansamar367@gmail.com>
2024-05-20 10:48:57 +0200

    Upsated xl:ml-[10vw]

--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -6,7 +6,7 @@ import Gaming from "../assets/image-gaming-growth.jpg";
 const Testimonails = () => {
   return (
     // max-w-7xl will  xl:max-w-7xl md:max-w-5xl px-2 apply a maximum width to the element on larger screens
-    <div className=" flex items-center justify-between mt-10 mb-12 mx-auto   text-left cursor-pointer md:ml-[7vw]   xl:ml-[9vw]   ">
+    <div className=" flex items-center justify-between mt-10 mb-12 mx-auto   text-left cursor-pointer md:ml-[7vw]   xl:ml-[10vw]   ">
       {/* Testimonials Container */}
       <div className="flex flex-col  md:flex-row md:space-x-6">
         {/* Retro */}
