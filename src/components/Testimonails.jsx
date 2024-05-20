0411cefb14b33cde0fef116d9c226009b1852f1a
Samar <osmansamar367@gmail.com>
2024-05-20 10:34:04 +0200

    Updated md:ml-[7vw] xl:ml-[10vw]

--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -6,7 +6,7 @@ import Gaming from "../assets/image-gaming-growth.jpg";
 const Testimonails = () => {
   return (
     // max-w-7xl will  xl:max-w-7xl md:max-w-5xl apply a maximum width to the element on larger screens
-    <div className="  ml-[10vw]  mt-10 mb-12 mx-auto px-2  text-left cursor-pointer md:max-w-5xl   ">
+    <div className="  mt-10 mb-12 mx-auto px-2  text-left cursor-pointer md:ml-[7vw] md:max-w-5xl  xl:ml-[10vw]   ">
       {/* Testimonials Container */}
       <div className="flex flex-col  md:flex-row md:space-x-6">
         {/* Retro */}
